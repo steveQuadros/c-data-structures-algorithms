f519a178b2510c62d748b26c65cec954b5a7486a
Steve Quadros <stevequadros@gmail.com>
2016-05-29 16:08:01 -0400

    Fixed issue in stack where popping an empty stack caused seg fault

--- stack.c
+++ stack.c
@@ -14,24 +14,24 @@ struct Node {
     struct Node *next;
 };
 
-struct Node *Pop(struct Stack* s) {
+void Pop(struct Stack* s) {
     struct Node *tmp = s->top;
+    if (tmp == NULL) return;
     free(s->top);
     s->top = tmp->next;
-    return s->top;
 }
 
-struct Node *Push(struct Stack *s, int data) {
-    struct Node *top = malloc(sizeof(struct Node));
+void Push(struct Stack *s, int data) {
+    struct Node *top = (struct Node*)malloc(sizeof(struct Node));
     top->data = data;
+
     struct Node *tmp = s->top;
     s->top = top;
     top->next = tmp;
-    return s->top;
 }
 
 struct Stack *Stack_Create() {
-    struct Stack *s = malloc(sizeof(struct Stack));
+    struct Stack *s = (struct Stack*)malloc(sizeof(struct Stack));
     s->top = NULL;
     return s;
 }
@@ -47,6 +47,8 @@ void Print(struct Stack *s) {
 
 int main(int argc, char *argv[]) {
     struct Stack *s = Stack_Create();
+
+    Pop(s);
 
     Push(s, 5);
     Print(s);
